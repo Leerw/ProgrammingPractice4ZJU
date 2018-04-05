4db75e2e981f88893e356e443f021409d2d1d19a
Leerw <lee.rw.cs@gmail.com>
2018-04-05 21:59:14 +0800

    A1042

---
+++ pat-a-practise/src/1042.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define N 54
+char mp[] = {'S', 'H', 'C', 'D', 'J'};
+int start[N + 1], end[N + 1], next[N + 1];
+
+int main() {
+    int K;
+    scanf("%d", &K);
+    for (int i = 1; i <= N; i++) {
+        start[i] = i;
+    }
+    for (int i = 1;  i <= N; i++) {
+        scanf("%d", &next[i]);
+    }
+    for (int step = 0; step < K; step++) {
+        for (int i = 1; i <= N; i++) {
+            end[next[i]] = start[i];
+        }
+        for (int i = 1; i <= N; i++) {
+            start[i] = end[i];
+        }
+    }
+    for (int i = 1; i <= N; i++) {
+        if (i != 1) {
+            printf(" ");
+        }
+        start[i]--;
+        printf("%c%d", mp[start[i] / 13], start[i] % 13 + 1);
+    }
+    return 0;
+}

a1404f262680b364d87944de98db708368272241
Leerw <lee.rw.cs@gmail.com>
2018-08-23 13:16:35 +0800

    format code

--- pat-a-practise/src/1017.cpp
+++ pat-a-practise/src/1017.cpp
@@ -5,11 +5,10 @@
 using namespace std;
 
 struct Customer {
-    int arriveH, arriveM, arriveS;
     int arriveT;
     int processT;
     int waitT;
-    Customer (int h, int m, int s, int t) : arriveH(h), arriveM(m), arriveS(s) { arriveT = h * 3600 + m * 60 + s;}
+    Customer (int h, int m, int s, int t) : processT(t*60) { arriveT = h * 3600 + m * 60 + s;}
 };
 
 struct Window {
@@ -27,6 +26,7 @@ int main(int argc, char const* argv[]) {
     queue<Customer> waitline;
     vector<Window> windows(k);
     int h, m, s, t;
+    int openT = 8 * 3600;
     int closeT = 17 * 3600;
     for (int i = 0; i < n; i++) {
         scanf("%d:%d:%d %d", &h, &m, &s, &t);
@@ -37,24 +37,27 @@ int main(int argc, char const* argv[]) {
         customers.push_back(customer);
     }
     std::sort(customers.begin(), customers.end(), [](Customer a, Customer b) {
-        return a.arriveH*3600 + a.arriveM*60 + a.arriveS < b.arriveH*3600 + b.arriveM*60 + b.arriveS;
+        return a.arriveT < b.arriveT;
     });
     for (auto customer : customers) {
         waitline.push(customer);
     }
     while (!waitline.empty()) {
-        for (int windex = 0; windex < k; windex = (windex + 1) % k) {
-            Customer tmp = waitline.front();
-            if (windows[windex].endT < tmp.arriveT) {
-                windows[windex].startT = tmp.arriveT;
-                windows[windex].processT = tmp.processT;
-                windows[windex].endT = windows[windex].startT + windows[windex].processT;
-                tmp.waitT = windows[windex].endT - tmp.arriveT;
-                waitT.push_back(tmp.waitT);
-            }
+        auto windex = min_element(windows.begin(), windows.end(), [](Window a, Window b) {
+            return a.endT < b.endT;
+        });
+        Customer tmp = waitline.front();
+        waitline.pop();
+        windex->startT = windex->endT;
+        if (tmp.arriveT > windex->startT) {
+            windex->startT = tmp.arriveT;
         }
+        windex->processT = tmp.processT;
+        windex->endT = windex->startT + windex->processT;
+        tmp.waitT = windex->startT - tmp.arriveT;
+        waitT.push_back(tmp.waitT);
     }
-    double averageWaitT = accumulate(waitT.begin(), waitT.end(), 0) / waitT.size();
+    double averageWaitT = accumulate(waitT.begin(), waitT.end(), 0) / 60.0 / waitT.size();
     printf("%.1f\n", averageWaitT);
     return 0;
 }

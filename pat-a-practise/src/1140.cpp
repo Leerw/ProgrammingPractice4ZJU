a987e898fda54c3187dc5e3733e65541764b72c5
Leerw <lee.rw.cs@gmail.com>
2018-09-04 16:48:57 +0800

    TLE

--- pat-a-practise/src/1140.cpp
+++ pat-a-practise/src/1140.cpp
@@ -1,27 +1,29 @@
+#include <cstdio>
 #include <iostream>
-#include <string>
 using namespace std;
-int d, n;
+const int maxn = 100010;
+int n, noindex = 0;
+int hashtable[maxn] = {0};
 int main() {
-    cin >> d >> n;
-    n--;
-    string s = std::to_string(d);
-    while (n--) {
-        string ans;
-        int i = 0, j, cnt = 0;
-        char c;
-        while (i < s.length()) {
-            c = s[i];
-            ans += c;
-            j = i;
-            while (s[i] == s[i + 1] && i + 1 < s.length()) { i++; }
-            int len = i - j + 1;
-            ans += std::to_string(len);
-            i++;
-        }
-        s = ans;
+  int temp, step = 0;
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &temp);
+    hashtable[temp] = i;
+  }
+  while (true) {
+    while (hashtable[0] != 0) {
+      swap(hashtable[0], hashtable[hashtable[0]]);
+      step++;
     }
-    s.replace(0, 1, std::to_string(d));
-    cout << s;
-    return 0;
+    noindex = 0;
+    while (noindex == hashtable[noindex] && noindex < n) {
+      noindex++;
+    }
+    if (noindex == n) break;
+    swap(hashtable[noindex], hashtable[0]);
+    step++;
+  }
+  printf("%d", step);
+  return 0;
 }
